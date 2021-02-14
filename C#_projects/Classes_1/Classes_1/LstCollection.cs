df8ea7c8ebcd9625d39caa9c6dd38ca0c38903c2
sophiashuv <shuvar@ucu.edu.ua>
2021-02-15 00:07:44 +0200

    directory refactoring

---
+++ C#_projects/Classes_1/Classes_1/LstCollection.cs
@@ -0,0 +1,145 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace Classes_1
+{
+    public class LstCollection
+        /* Class for Collection on list representation. */
+
+    {
+        private List<Product> lst;
+
+        public LstCollection() => this.lst = new List<Product>();
+
+        public LstCollection(List<Product> lst) => this.lst = new List<Product>(lst);
+
+        public override string ToString()
+        {
+            string res = "";
+            foreach (Product element in lst)
+                res += element.ToString() + "\n=\n";
+            return "\n" + res.Substring(0, res.Length - 3);
+        }
+
+        public int Length() => lst.Count;
+        
+        public Product this[int i]
+        {
+            get => lst[i];
+            set => lst[i] = value;
+        }
+
+        public LstCollection AddItem(Product item)
+        {
+            lst.Add(item);
+            return this;
+        }
+
+        public LstCollection Sort(string field)
+        {
+            try { this.lst = lst.OrderBy(e => e.GetType().GetProperty(field).GetValue(e, null)).ToList(); }
+            catch (Exception) {throw new ArgumentException("Wrong Field name!");}
+            return this;
+        }
+
+        public List<Product> Search(string subStr)
+        {
+            bool SearchPredicater(Product e)
+            {
+                foreach (PropertyDescriptor prop in TypeDescriptor.GetProperties(e)) 
+                    if (prop.GetValue(e).ToString().Contains(subStr)) return true;
+                return false;
+            }
+
+            var filtered = Validation.ValidateSearch(lst.FindAll(SearchPredicater));
+            return filtered;
+        }
+
+        public LstCollection Delete(string id)
+        {
+            var n = lst.Find(x => x.Id == id);
+            if (n != null) lst.Remove(n);
+            return this;
+        }
+        
+        public LstCollection Edit(string id, string atter, string value)
+        {
+            var n = lst.Find(x => x.Id == id);
+            if (n != null)
+            {
+                PropertyInfo propertyInfo = Validation.ValidateEdit(n.GetType().GetProperty(atter));
+                propertyInfo.SetValue(n, Convert.ChangeType(value, propertyInfo.PropertyType), null);
+            }
+            return this;
+        }
+        
+        public LstCollection ReadTxtFile(string fileName)
+        {
+            var fileData = System.IO.File.ReadAllText(fileName).Split('=').ToList();
+            foreach (var strObject in fileData) {
+                try
+                {
+                    Product p = new Product();
+                    var lstObjects = strObject.Substring(1, strObject.Length - 2).
+                        Split('\n').ToList();
+                    foreach (var field in lstObjects)
+                    {
+                        var fieldValue = field.Split(':').ToList();
+                        PropertyInfo propertyInfo = p.GetType().GetProperty(char.
+                            ToUpper(fieldValue[0][0]) + fieldValue[0].Substring(1));
+                        propertyInfo.SetValue(p,
+                            Convert.ChangeType(fieldValue[1].Substring(1), propertyInfo.PropertyType), null);
+                    }
+                    if (string.IsNullOrEmpty(p.Id)) p.Id = Guid.NewGuid().ToString();
+                    this.AddItem(p);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"--{e.InnerException.Message}");
+                    continue;
+                }
+            }
+            return this;
+        }
+        
+        public void WriteTxtFile(string fileName)
+        {
+            using(TextWriter tw = new StreamWriter(fileName)) tw.WriteLine(this);
+        }
+        
+        public void AddToTxtFile(string fileName)
+        {
+            using (StreamWriter tw = File.AppendText(fileName)) tw.WriteLine("=" + this.ToString());
+        }
+        
+        public LstCollection ReadJsonFile(string fileName)
+        {
+            using (StreamReader r = new StreamReader(fileName))
+            {
+                string json = r.ReadToEnd();
+                var dictionarys = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(json);
+                foreach (var v in dictionarys)
+                {
+                    try
+                    {
+                        Product p = JsonConvert.DeserializeObject<Product>(JsonConvert.SerializeObject( v ));
+                        this.AddItem(p);
+                    }
+                    catch (Exception e) { Console.WriteLine($"--{e.InnerException.Message}"); }
+                }
+            }
+            return this;
+        }
+        
+        public void WriteJsonFile(string fileName)
+        {
+            string json = JsonConvert.SerializeObject(this.lst.ToArray());
+            System.IO.File.WriteAllText(fileName, json);
+        }
+    }
+}
---
+++ C#_projects/Classes_1/Classes_1/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace Classes_1
+{
+    class MainClass
+    {
+        private static void read_txt_file(LstCollection l)
+        {
+            Console.WriteLine("Enter file_name: ");
+            var file = Validation.ValidateFile(Console.ReadLine());
+            if (file.EndsWith(".txt")) l.ReadTxtFile(file);
+            else l.ReadJsonFile(file);
+        }
+        
+        private static void sort_elements(LstCollection l)
+        {
+            Console.WriteLine("Enter field for which you want to sort: \n" +
+                              "POSSIBLE: Title, Image_url, Price, Created_at, " +
+                              "Updated_at, Description, Id:\n");
+            l.Sort(Console.ReadLine());
+        }
+        
+        private static void search_elements(LstCollection l)
+        {
+            Console.WriteLine("Enter parameter which elements you want to find: \n");
+            var res = new LstCollection(l.Search(Console.ReadLine()));
+            Console.WriteLine(res);
+        }
+        
+        private static void add_elements(LstCollection l)
+        {
+            var p = new Product();
+            p.InputProduct();
+            l.AddItem(p);
+        }
+        
+        private static void del_product(LstCollection l)
+        {
+            Console.WriteLine("Enter id to delete: ");
+            l.Delete(Console.ReadLine());
+        }
+        
+        private static void edit_product(LstCollection l)
+        {
+            Console.Write("Enter id to edit: ");
+            var id = Console.ReadLine();
+            Console.Write("Enter atter to edit: ");
+            var atter = Console.ReadLine();
+            Console.Write("Enter value to change: ");
+            var value = Console.ReadLine();
+            l.Edit(id, atter, value);
+        }
+        
+        private static void write_file(LstCollection l)
+        {
+            Console.WriteLine("Enter file_name: ");
+            var file = Validation.ValidateFile(Console.ReadLine());
+            if (file.EndsWith(".txt")) l.WriteTxtFile(file);
+            else l.WriteJsonFile(file);
+        }
+        
+        private static void add_txt_file(LstCollection l)
+        {
+            Console.WriteLine("Enter file_name: ");
+            l.AddToTxtFile(Validation.ValidateFile(Console.ReadLine()));
+        }
+        
+        private static string get_help_message()
+        {
+            string helpMessage = new string('*', 51);
+            helpMessage += "\n*  HELP:" + new string(' ', 42) + "*\n*  Possible commands:" + new string(' ', 29) +
+                            "*\n*  1 - to read from file;" + new string(' ', 25) +
+                            "*\n*  2 - to sort elements; " + new string(' ', 25) +
+                            "*\n*  3 - to to search element.  " + new string(' ', 20) +
+                            "*\n*  4 - to to add Product to collection. " + new string(' ', 10) +
+                            "*\n*  5 - to del element from collection.  " + new string(' ', 10) +
+                            "*\n*  6 - to edit element from collection.  " + new string(' ', 9) +
+                            "*\n*  7 - to write collection elements to file.      " +
+                            "*\n*  8 - to add  collection elements to txt file.   " +
+                            "*\n*  9 - to print collection. " + new string(' ', 22) +
+                            "*\n*  exit - to exit.  " + new string(' ', 30) + "*\n";
+            helpMessage += new string('*', 51) + "\n";
+            return helpMessage;
+        }
+
+        public static void Main(string[] args)
+        {
+            LstCollection l = new LstCollection();
+            while (true) 
+            {
+                Console.WriteLine(get_help_message());
+                string task = Console.ReadLine();
+                switch (task)
+                {
+                    case "1":
+                        Validation.ValidateInput(l, read_txt_file);
+                        break;
+                    case "2":
+                        Validation.ValidateInput(l, sort_elements);
+                        break;
+                    case "3":
+                        Validation.ValidateInput(l, search_elements);
+                        break;
+                    case "4":
+                        Validation.ValidateInput(l, add_elements);
+                        break;
+                    case "5":
+                        Validation.ValidateInput(l, del_product);
+                        break;
+                    case "6":
+                        Validation.ValidateInput(l, edit_product);
+                        break;
+                    case "7":
+                        Validation.ValidateInput(l, write_file);
+                        break;
+                    case "8":
+                        Validation.ValidateInput(l, add_txt_file);
+                        break;
+                    case "9":
+                        Console.WriteLine(l);
+                        break;
+                    case "exit":
+                        Console.WriteLine("GOODBYE!");
+                        return;
+                    default:
+                        Console.WriteLine("WRONG INPUT!");
+                        continue;
+            }
+                Console.WriteLine();
+            } 
+        }
+    }
+}
