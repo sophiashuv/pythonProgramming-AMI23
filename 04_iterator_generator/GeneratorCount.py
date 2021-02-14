b8c734ce4edb600f2462584314e030473754d409
sophiashuv <shuvar@ucu.edu.ua>
2021-02-14 23:30:22 +0200

    directory refactoring

--- 04_iterator_generator/GeneratorCount.py
+++
@@ -1,9 +0,0 @@
-from coprime_chacks import IsCoprime
-
-
-def coprimes_generator(low, high, first_el):
-    while high != 0:
-        if IsCoprime.is_coprime(first_el, low):
-            high -= 1
-            yield (first_el, low)
-        low += 1
--- 10_RESTful_API/crud.py
+++
@@ -1,8 +0,0 @@
-from app import *
-from routes import *
-
-
-if __name__ == '__main__':
-    db.create_all()
-    db.session.commit()
-    app.run(debug=True, host='127.0.0.1')
