dd257a2e0f08d29287a586c56c6edf5f2e13e515
sophiashuv <shuvar@ucu.edu.ua>
2021-04-05 17:35:23 +0300

    add task04 API

---
+++ C#_projects/API_05/API_05/Authentication/CustomPasswordHasher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace API_05.Authentication
+{
+    public class CustomPasswordHasher
+    {
+        public CustomPasswordHasher()
+        {
+        }
+    }
+}
---
+++ C#_projects/API_05/API_05/Models/UserRole.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace API_05.Controllers
+{
+    public class UserRole
+    {
+        public UserRole()
+        {
+        }
+    }
+}
